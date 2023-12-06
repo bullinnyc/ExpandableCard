f84c7f7143189f0000a60542463a7b5854bb311b
bullinnyc <i8u11@icloud.com>
2023-12-06 12:06:11 +0700

    Initial Commit

---
+++ ExpandableCard/ExpandableCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpandableCardApp.swift
+//  ExpandableCard
+//
+//  Created by Dmitry Kononchuk on 06.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ExpandableCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
